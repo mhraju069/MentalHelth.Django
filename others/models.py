1ee79809c011aa41f421aa934e28871a426f3f71
Raju <mhraju069@gmail.com>
2026-03-12 01:12:49 +0600

    update: add sore on DailyReport model

--- others/models.py
+++ others/models.py
@@ -12,8 +12,22 @@ class DailyReport(models.Model):
     assesment = models.CharField(max_length=100, choices=LIST)
     time = models.DateTimeField()
     journal = models.TextField()
+    score = models.IntegerField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.time} - {self.assesment}"
+
+    def save(self, *args, **kwargs):
+        if self.assesment == 'excellent':
+            self.score = 10
+        elif self.assesment == 'good':
+            self.score = 8
+        elif self.assesment == 'neutral':
+            self.score = 6
+        elif self.assesment == 'sad':
+            self.score = 4
+        elif self.assesment == 'depressed':
+            self.score = 2
+        super().save(*args, **kwargs)
